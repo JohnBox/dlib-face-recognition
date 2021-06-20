20181e77432b4e61e242830598d282d29221f3f3
johnbox <johnboxwon@gmail.com>
2021-06-21 01:06:24 +0300

    add names and encodings methods

--- src/face_encoding/compare.rs
+++ src/face_encoding/compare.rs
@@ -6,31 +6,39 @@ use super::encoding::FaceEncoding;
 pub struct FaceComparer {
     seed: usize,
     names: HashMap<usize, String>,
-    values: HashMap<usize, FaceEncoding>,
+    encodings: HashMap<usize, FaceEncoding>,
 }
 
 impl FaceComparer {
-    pub fn new(names: Vec<String>, values: Vec<FaceEncoding>) -> Self {
-        assert_eq!(names.len(), values.len());
+    pub fn new(names: Vec<String>, encodings: Vec<FaceEncoding>) -> Self {
+        assert_eq!(names.len(), encodings.len());
 
         let seed = names.len();
-        let names = (0..seed-1).zip(names).collect();
-        let values = (0..seed-1).zip(values).collect();
+        let names = (0..seed - 1).zip(names).collect();
+        let encodings = (0..seed - 1).zip(encodings).collect();
 
         FaceComparer {
             seed,
             names,
-            values,
+            encodings,
         }
+    }
+
+    pub fn names(&self) -> Vec<String> {
+        self.names.values().cloned().collect()
+    }
+
+    pub fn encodings(&self) -> Vec<FaceEncoding> {
+        self.encodings.values().cloned().collect()
     }
 
     pub fn insert(&mut self, name: String, value: FaceEncoding) {
         let name_str = name.as_str();
         if let Some((&key, _)) = self.names.iter().find(|(_, n)| n.as_str() == name_str) {
-            self.values.insert(key, value);
+            self.encodings.insert(key, value);
         } else {
             self.names.insert(self.seed, name);
-            self.values.insert(self.seed, value);
+            self.encodings.insert(self.seed, value);
             self.seed += 1;
         }
     }
@@ -39,7 +47,7 @@ impl FaceComparer {
         const TOLERANCE: f64 = 0.6;
 
         if let Some((key, x)) = self
-            .values
+            .encodings
             .iter()
             .map(|(i, f)| (i, f.distance(face)))
             .min_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap())
@@ -60,7 +68,7 @@ impl FaceComparer {
 
     pub fn remove_key(&mut self, key: &usize) {
         self.names.remove(key);
-        self.values.remove(key);
+        self.encodings.remove(key);
     }
 
     pub fn remove_name(&mut self, name: &str) {
